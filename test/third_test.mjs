7ce8de718f3287c6d00c74b377467162216bd7a8
William Ashton <William@AshtonFam.org>
2020-02-10 13:55:15 -0500

    Add the third implementation

---
+++ test/third_test.mjs
@@ -0,0 +1,36 @@
+import test from 'ava';
+
+import Triangle from '../lib/third.mjs';
+
+/*
+ * WARNING: Different interface. See next file for restored interface.
+ */
+test('viewing side lengths', t => {
+  const triangle = Triangle.create(3, 4, 5);
+
+  t.is(triangle.a, 3);
+  t.is(triangle.b, 4);
+  t.is(triangle.c, 5);
+});
+
+test('checking triangle-ness', t => {
+  const right = Triangle.create(3, 4, 5);
+  const bad = Triangle.create(1, 2, 3);
+
+  t.true(Triangle.isTriangle(right));
+  t.false(Triangle.isTriangle(bad));
+});
+
+test('works without new', t => {
+  const right = Triangle.create(3, 4, 5);
+
+  t.true(Triangle.isTriangle(right));
+});
+
+test('can be changed', t => {
+  const right = Triangle.create(3, 4, 5);
+
+  right.a = 1;
+
+  t.false(Triangle.isTriangle(right));
+});
