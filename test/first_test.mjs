88f56b2905289ff70cb20049c07e83ef51a42ab8
William Ashton <William@AshtonFam.org>
2020-02-10 13:47:47 -0500

    Add first implementation

--- test/first_test.mjs
+++ test/first_test.mjs
@@ -1,5 +1,25 @@
 import test from 'ava';
 
+import Triangle from '../lib/first.mjs';
+
 test('viewing side lengths', t => {
-  t.pass();
+  const triangle = new Triangle(3, 4, 5);
+
+  t.is(triangle.a, 3);
+  t.is(triangle.b, 4);
+  t.is(triangle.c, 5);
+});
+
+test('checking triangle-ness', t => {
+  const right = new Triangle(3, 4, 5);
+  const bad = new Triangle(1, 2, 3);
+
+  t.true(right.isTriangle());
+  t.false(bad.isTriangle());
+});
+
+test('works without new', t => {
+  const right = Triangle(3, 4, 5);
+
+  t.true(right.isTriangle());
 });
